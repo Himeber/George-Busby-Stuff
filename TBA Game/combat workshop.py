4fb77bb9a0b15cf89888fc5502cd38d3a185f1b4
Himeber <georgebusby08@gmail.com>
2024-10-02 11:48:15 -0600

    workshopping

--- TBA Game/combat workshop.py
+++ TBA Game/combat workshop.py
@@ -2,7 +2,6 @@
 import random
 import time
 import sys
-
 # functions
 def cs():
     print('\033c')
@@ -39,8 +38,63 @@ def line():
     return "-----------------------"
 def combat(enemy):
     global player
+    pstunned = False
+    estunned = False
+    turn = "player"
     while player.hp > 0 and enemy.hp > 0:
-        pass
+        if turn == "player":
+            if pstunned:
+                timeprint("You are stunned and cannot act.")
+                pstunned = False
+                turn = "enemy"
+            else:
+                timeprint("It is your turn.")
+                action = strput("What would you like to do?").lower
+                if action == "attack":
+                    damage = round(player.atk * (0.75 + random.random()),1)-enemy.defence
+                    timeprint("You hit the enemy for " + str(damage - enemy.defence) + " damage.")
+                    enemy.hp = enemy.hp - damage
+                    timeprint("The " + enemy.name + " has " + str(enemy.hp) + " hp.")
+                else:
+                    if action in player.skills:
+                        ability = player.skills(action)
+                        damage = ability.damage * (1+(player.damage/10))
+                        try:
+                            if ability.heal >= 0:
+                                heal = ability.heal
+                        except:
+                            if ability.heal == "lifesteal":
+                                heal = ability.damage
+                        if ability.crit:
+                            critchance = ability.critchance
+                        else:
+                            critchance = 0
+                        stun = ability.stun
+                        cost = ability.cost
+                        costtype = ability.costtype
+                        if costtype == "Energy":
+                            timeprint("You use " + ability.name + ".")
+                        elif costtype == "Mana":
+                            timeprint("You cast " + ability.name + ".")
+                        if damage != 0:
+                            damage -= enemy.defence
+                            if damage < 0:
+                                damage = 0
+                            enemy.hp = enemy.hp - damage
+                        timeprint("You dealt " + str(damage) + " damage.")
+                        timeprint("The " + enemy.name + " has " + str(enemy.hp) + " hp.")
+                        if heal > 0:
+                            if heal > (player.maxhp - player.hp):
+                                heal = player.maxhp - player.hp
+                            timeprint("You healed for " + str(heal) + " hp.")
+                            player.hp += heal
+                    else:
+                        timeprint("You do not have that ability.")
+        elif turn == "enemy":
+            if estunned:
+                timeprint("The enemy is stunned and cannot act.")
+                estunned = False
+                turn = "player"
 class Player:
     def __init__(self):
         self.name = None
@@ -49,22 +103,24 @@ class Player:
         self.defence = 0
         self.energy = 25
         self.mana = 25
+        self.skills = {
+
+        }
     def __repr__(self):
         thingy = line() + "\n" + self.name + "\n" + line()
         thingy += "\nHP: " + str(self.hp) + "/" + str(self.maxhp)
         thingy += "\nAttack: " + str(atk) + " | Defence: " + str(defence)
 class Enemy:
-    def __init__(self,name,hp,atk,defence):
+    def __init__(self,name="Bob",hp=10,atk=1,defence=0):
         self.name = name
         self.hp = hp
         self.maxhp = hp
         self.atk = atk
         self.defence = defence
 class Spell:
-    def __init__(self,name,cost=0,costtype="Energy",damage=0,defence=0,heal=0,stun=0,crit=0,critchance=0):
+    def __init__(self,name,cost=0,costtype="Energy",damage=0,heal=0,stun=0,crit=0,critchance=0):
         self.name = name
         self.damage = 0
-        self.defence = 0
         self.heal = 0
         self.cost = 0
         self.stun = False
@@ -97,8 +153,6 @@ fireball.damage = 25
 fireball.crit = True
 fireball.critchance = 25
 print(fireball)
-block = Spell("Block")
-block.defence = 5
 heal = Spell("Heal")
 heal.heal = 10
 bonk = Spell("Bonk")
